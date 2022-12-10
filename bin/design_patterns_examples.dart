afc9662e888c8de679090c901afa214332106581
ABHISHEK PATIL <abhishek.patil@joflee.com>
2022-12-10 10:56:20 +0530

    first commit

---
+++ bin/design_patterns_examples.dart
@@ -0,0 +1,5 @@
+import 'package:design_patterns_examples/design_patterns_examples.dart' as design_patterns_examples;
+
+void main(List<String> arguments) {
+  print('Hello world: ${design_patterns_examples.calculate()}!');
+}
---
+++ test/design_patterns_examples_test.dart
@@ -0,0 +1,8 @@
+import 'package:design_patterns_examples/design_patterns_examples.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
